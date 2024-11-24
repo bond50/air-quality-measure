9750fdbb88681ba205a8a68070250dd5039963aa
bobd50 <galavu10@gmail.com>
2024-11-24 17:52:03 +0300

    addesd units

--- project/city.php
+++ project/city.php
@@ -1,8 +1,10 @@
 <?php require __DIR__ . '/inc/functions.inc.php';
+
 $city = null;
 if (!empty($_GET['city'])) {
     $city = $_GET['city'];
 }
+
 $fileName = null;
 if (!empty($city)) {
     $cities = json_decode(
@@ -24,14 +26,35 @@ if (!empty($fileName)) {
         file_get_contents('compress.bzip2://' . __DIR__ . "/../data/" . $fileName),
         true)['results'];
 
+    $units = [
+        'pm25' => [],
+        'pm10' => [],
+    ];
+
     foreach ($results as $result) {
-        if ($result['parameter'] !== 'pm25') continue;
+        if (!empty($units['pm25']) && !empty($units['pm10'])) break;
+        if ($result['parameter'] === 'pm25') {
+            $units['pm25'] = $result['unit'];
+        }
+        if ($result['parameter'] === 'pm10') {
+            $units['pm10'] = $result['unit'];
+        }
+    }
+
+    foreach ($results as $result) {
+
+        if ($result['parameter'] !== 'pm25' && $result['parameter'] !== 'pm10') continue;
 
         $month = substr($result['date']['local'], 0, 7);
+
         if (!isset($stats[$month])) {
-            $stats[$month] = [];
+            $stats[$month] = [
+                'pm25' => [],
+                'pm10' => []
+            ];
         }
-        $stats[$month][] = $result['value'];
+        if ($result['value'] < 0) continue;
+        $stats[$month][$result['parameter']][] = $result['value'];
     }
 
 }
@@ -46,12 +69,24 @@ if (!empty($fileName)) {
 <?php else: ?>
     <?php if (!empty($stats)) : ?>
         <table>
+            <thead>
+            <tr>
+                <th>Month</th>
+                <th>PM 2.5 concentration</th>
+                <th>PM 10 concentration</th>
+            </tr>
+            </thead>
+            <tbody>
             <?php foreach ($stats as $month => $measurements) : ?>
                 <tr>
                     <th><?= e($month) ?></th>
-                    <td><?= e(array_sum($measurements) / count($measurements)) ?></td>
+                    <td><?= e(round(array_sum($measurements['pm25']) / count($measurements['pm25']), 2)) ?>
+                    <?= e($units['pm25'])?></td>
+                    <td><?= e(round(array_sum($measurements['pm10']) / count($measurements['pm10']), 2)) ?>
+                    <?= e($units['pm10'])?></td>
                 </tr>
             <?php endforeach; ?>
+            </tbody>
         </table>
     <?php endif; ?>
 <?php endif; ?>
